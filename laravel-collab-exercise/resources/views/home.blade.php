17860f8d9161138c7a72c4fda1585e69b4d4924e
Rodrigo Lopez <scdtp2019@gmail.com>
2023-05-22 19:42:24 +0800

    Add front end for profile Hard coded information

--- laravel-collab-exercise/resources/views/home.blade.php
+++ laravel-collab-exercise/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +19,21 @@
             </div>
         </div>
     </div>
+</div> -->
+
+
+<div class="container">
+<div class="row d-flex">
+            <div class="col-4">
+                <img 
+                src="https://img.rawpixel.com/private/static/images/website/2022-05/ns8230-image.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=348b2fd5c7adbc576517dae7c32de4aa" 
+                alt="profile"
+                class="rounded-circle" style="width:20vw"/>
+            </div>
+            <div class="col-1">
+                <h1 class="username text-uppercase">TestUser1</h1>
+                <p class="user">@test_user</p>
+            </div>
+        </div>
 </div>
 @endsection
