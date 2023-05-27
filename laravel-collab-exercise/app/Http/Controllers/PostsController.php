c2586473bbe0f6a8509b164cf687893c345391c9
Rodrigo Lopez <scdtp2019@gmail.com>
2023-05-27 09:42:18 +0800

    Add some notes

--- laravel-collab-exercise/app/Http/Controllers/PostsController.php
+++ laravel-collab-exercise/app/Http/Controllers/PostsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+// use Intervention 
+
 class PostsController extends Controller
 {
 
@@ -23,11 +25,22 @@ class PostsController extends Controller
         $data = request()->validate([
             'caption' => 'required',
             'image' => 'required | image',
-
         ]);
+
+        // Store the image request in a variable
+
+        // Setup Intervention > Setup the namespace 
+
+        // Setup an array for caption key value pair to access the array inside of the $data
+
         //create() is a built-in hasFactory method
         auth()->user()->posts()->create($data);
 
+        // comment ouot this test die and dump method
         dd(request()->all());
+
+        // Redirection to the /profile/ path for the authenticate user using user id
+
+
     }
 }
