c9745cd5ada249752c83b4fa70c47bb2b6c645f0
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-01 20:01:00 +0800

    setting up profiles controller edit wip

--- laravel-collab-exercise/app/Http/Controllers/ProfilesController.php
+++ laravel-collab-exercise/app/Http/Controllers/ProfilesController.php
@@ -3,33 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
-
 
 class ProfilesController extends Controller
 {
     public function index(User $user)
     {
-			// $user = User::findOrFail($user); 
-			// return view('profiles.index', ['user' => $user]);
-            return view('profiles.index', compact('user'));
+        // $user = User::findOrFail($user);
+        // return view('profiles.index', ['user' => $user]);
+        return view('profiles.index', compact('user'));
     }
 
-    public function edit(\App\Models\User $user)
+    public function edit(User $user)
     {
-            return view('profiles.edit', compact('user'));
+        return view('profiles.edit', compact('user'));
     }
 
-    public function update()
+    public function update(User $user)
     {
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
             'url' => 'url',
-            'image' => '',
+            // 'image' => '',
         ]);
 
-        dd($data);
-    }
+        $user->profile->update($data);
 
+        return redirect("/profile/{$user->id}");
+        // dd($data);
+    }
 }
--- laravel-collab-exercise/app/Models/Profile.php
+++ laravel-collab-exercise/app/Models/Profile.php
@@ -2,17 +2,16 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Profile extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
-		public function user() 
-		{
-			return $this->belongsTo(User::class);
-		}
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
