980b931ea3df40b08678e359d4d6bb66d2ffff99
Rodrigo Lopez <scdtp2019@gmail.com>
2023-06-02 19:42:18 +0800

    setup image column in profiles table
    setup array_merge for data request and imagePath for intervention

--- laravel-collab-exercise/app/Http/Controllers/ProfilesController.php
+++ laravel-collab-exercise/app/Http/Controllers/ProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -26,10 +27,29 @@ class ProfilesController extends Controller
             'title' => 'required',
             'description' => 'required',
             'url' => 'url',
-            // 'image' => '',
+            'image' => '',
         ]);
 
-        auth()->user()->profile->update($data);
+        if(request('image')){
+            $imagePath = request('image')->store('profile', 'public');
+
+            // Setup Intervention > Setup the namespace
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(500, 500, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+    
+            $image->save();
+        }
+
+        // dd($data);
+        dd(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
         // dd($data);
