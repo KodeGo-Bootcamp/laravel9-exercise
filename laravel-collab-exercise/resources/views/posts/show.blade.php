f6ac71d0c4dd849ec06881d9702c19fb9cb71570
Rodrigo Lopez <scdtp2019@gmail.com>
2023-06-03 09:31:36 +0800

    Setup profile pic in post image page

--- laravel-collab-exercise/resources/views/posts/show.blade.php
+++ laravel-collab-exercise/resources/views/posts/show.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="container">
-    <div class="row">
+    <!-- <div class="row">
         <h1>Photo of Posts</h1>
         <div class="col-8">
             <img src="/storage/{{$post->image}}" alt="">
@@ -13,7 +13,26 @@
             <h3>{{$post->user->username}}</h3>
             <p>{{$post->caption}}</p>
         </div>
-    </div>
+    </div> -->
      
+
+    <div class="row">
+        <div class="col-8">
+             <img src="/storage/{{$post->image}}" alt="">
+        </div>
+        <div class="col-4">
+            <div>
+                <div>
+                    <div>
+                        <img src="/storage/{{$post->user->profile->image}}" alt="" class="rounded-circle" style="width:20vw">
+                    </div>
+                    <div>
+                        <h4>{{$post->user->username}}</h4>
+                    </div>
+                </div>
+                <p>{{$post->caption}}</p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
